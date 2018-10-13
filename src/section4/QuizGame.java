9f9ebdc803427a95257b70e6e8bd869ab80cb397
MadMcP <league@wts14-4.attlocal.net>
2018-10-13 11:53:47 -0700

    Workshop 3

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,30 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
-	
+
 	public static void main(String[] args) {
-		
-		// 1.  Create a variable to hold the user's score 
-		
-		// 2.  Ask the user a question 
-		
-		// 3.  Use an if statement to check if their answer is correct
-		
-		// 4.  if the user's answer is correct
-		
-		// -- add one to their score 
-		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
-		
+
+		// 1. Create a variable to hold the user's score
+		int score = 0;
+		String preamble;
+		// 2. Ask the user a question
+		preamble = JOptionPane.showInputDialog("Introduction");
+		// 3. Use an if statement to check if their answer is correct
+		if (preamble.equals("Preamble")) {
+			JOptionPane.showMessageDialog(null, "Correct! Score increased by 10");
+			score = score + 10;
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong... No score increase.");
+		}
+		// 4. if the user's answer is correct
+
+		// -- add one to their score
+
+		// 5. Create more questions by repeating steps 1, 2, and 3 below.
+
+		// 6. After all the questions have been asked, print the user's score
+
 	}
 }
